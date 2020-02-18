3023be55354f8abb5a6822454e7c9c2eac99c025
abc <abc@example.com>
2020-02-18 07:36:18 +0000

    still in progress

---
+++ 8.js
@@ -0,0 +1,14 @@
+const scroll = new SmoothScroll('#navbar a[href*="#"]', {
+	speed: 2500,
+	// offset: 50
+});
+
+const scroll2 = new SmoothScroll('#cover a[href*="#"]', {
+	speed: 2500,
+	// offset: 50
+});
+
+const scroll3 = new SmoothScroll('#contents a[href*="#"]', {
+	speed: 2500,
+	// offset: 50
+});
